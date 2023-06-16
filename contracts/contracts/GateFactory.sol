f1c4c31a0b6069b0f76bc209efeb280550489d80
berkingurcan <berkingurcan@yandex.com>
2023-06-16 15:47:26 +0300

    owner

--- contracts/contracts/GateFactory.sol
+++ contracts/contracts/GateFactory.sol
@@ -12,8 +12,8 @@ contract GateFactory {
      * @param symbol symbol of the Gate contract
      * @return address of the new Gate contract
      */
-    function deployGate(string memory name, string memory symbol) public returns (address) {
-        GateToken gate = new GateToken(name, symbol);
+    function deployGate(address owner, string memory name, string memory symbol) public returns (address) {
+        GateToken gate = new GateToken(owner, name, symbol);
         emit GateDeployed(msg.sender, address(gate));
         return address(gate);
     }
--- contracts/contracts/GateToken.sol
+++ contracts/contracts/GateToken.sol
@@ -7,9 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GateToken is ERC20, ERC20Burnable, Ownable {
     constructor(
+        address _owner,
         string memory _gateName,
         string memory _gateSymbol
-    ) ERC20(_gateName, _gateSymbol) {}
+    ) ERC20(_gateName, _gateSymbol) {
+        transferOwnership(_owner);
+    }
 
     function mint(address to) public onlyOwner {
         _mint(to, 1 * 10 ** 18);
@@ -21,5 +24,17 @@ contract GateToken is ERC20, ERC20Burnable, Ownable {
         uint256 amount
     ) internal virtual override {
         revert("No transfer");
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        revert("No transfer");
+    }
+
+    function burnFromVerifier(address account) public onlyOwner {
+        _burn(account, 1 * 10 ** 18);
     }
 }
